4245ed5b3e5a5aec949a4f9b7fe145d7246cefab
Byambasuren11 <byumbasurenbold@gmail.com>
2024-12-02 16:42:06 +0800

    data types

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("hello-world");
+//data types
+//1. String=> text type=> " text bicin"
+//2. Number=> number=>1 2 3
+//3. Boolean=> true or false=> true, false
+//4. Object=>{key : value}
+//5. Array=> [ index, index  ]
+//6. Null or Undefined =>  no value
+
